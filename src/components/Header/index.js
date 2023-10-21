97417e7ae6718d902794d162d4da2c427007bba3
Anuja-Aliveli <a.anuja54@gmail.com>
2023-10-21 07:08:52 +0000

    Time section completed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,15 @@ import "./index.css";
 const Header = () => {
   const [tickets, setTickets] = useState(1);
   const [ticketType, setTicketType] = useState("Standard");
+  
+  const handleType = (event) => {
+    setTicketType(event.target.value);
+  }
+
+  const handleTicket = (event) => {
+    setTickets(event.target.value);
+  }
+
   return (
     <div className="page-background">
       <div className="header-container">
@@ -12,19 +21,15 @@ const Header = () => {
             <h4>Mad (2023)&nbsp;</h4>
             <h4 className="circle">UA</h4>
           </div>
-          <p className="location">Imax: Hyderabad| Today, 21 Oct, 02:00 PM</p>
+          <p className="location">Imax: Hyderabad | Today, 21 Oct, 02:00 PM</p>
         </div>
         <div className="right-container">
-          <select>
-            <option value="Standard" selected>
-              Standard
-            </option>
+          <select onChange={handleType} value={ticketType}>
+            <option value="Standard">Standard</option>
             <option value="Premium">Premium</option>
           </select>
-          <select>
-            <option value={1} selected>
-              1 Ticket
-            </option>
+          <select onChange={handleTicket} value={tickets}>
+            <option value={1}>1 Ticket</option>
             <option value={2}>2 Tickets</option>
             <option value={3}>3 Tickets</option>
             <option value={4}>4 Tickets</option>
@@ -32,6 +37,11 @@ const Header = () => {
             <option value={6}>6 Tickets</option>
           </select>
         </div>
+      </div>
+      <div className="time-container">
+          <button className="time active" type="button">02:00</button>
+          <button className="time" type="button" disabled>06:00</button>
+          <button className="time" type="button" disabled>09:00</button>
       </div>
     </div>
   );
